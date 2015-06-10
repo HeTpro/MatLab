ee864b4f59b91cb9a6ad0baaa60ab54ee542b5c4
esmarr58 <ruben.estrada@hetpro.com.mx>
2015-06-09 20:59:26 -0500

    Create S1E1.m

---
+++ Introduccion/S1E1.m
@@ -0,0 +1,9 @@
+clear all
+clc
+x = 0;
+x = input('Ingresa un valor entre 0 y 90')
+disp = ('El dato que ingresaste es: ')
+x
+disp = ('El dato ingresado convertido a radianes: ')
+radianes = x*pi/180
+
